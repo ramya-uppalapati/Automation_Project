09f553c557766c399cffac5fa34d1a3913741d5f
ramya-uppalapati <79410444+ramya-uppalapati@users.noreply.github.com>
2022-02-27 21:05:20 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -6,6 +6,9 @@ pathname=/tmp/
 timestamp=$(date '+%d%m%Y-%H%M%S')
 logname=${service}-httpd-logs-${timestamp}.tar
 file=${pathname}${logname}
+sitepath="/var/www/html"
+cronpath=/etc/cron.d/Automation
+
 sudo apt update -y
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install $pkgs
@@ -33,3 +36,19 @@ if test -f "$file"; then
     echo "completed upload";
 fi
 
+if test -f "${sitepath}/inventory.html"; then
+    echo "inventory exists";
+else
+    echo "creating inventory";
+    echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' >{doc}/inventory.html
+fi
+
+if test -f "${sitepath}/inventory.html"; then
+    size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/invetntory.html
+fi
+
+if [[ ! -f ${cronpath} ]];
+then
+    echo " * * * * * root/Automation_Project/automation.sh" >> ${cronpath}
+fi
